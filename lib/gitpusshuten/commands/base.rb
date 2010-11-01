8a43013c6c64d32f961586036ca18dc273349c43
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-01 22:18:16 +0100

    Added a base class for commands to extend from.

---
+++ lib/gitpusshuten/commands/base.rb
@@ -0,0 +1,15 @@
+module GitPusshuTen
+  module Commands
+    class Base
+      
+      ##
+      # Command-line Interface
+      attr_accessor :cli
+      
+      ##
+      # Configuration (Environment)
+      attr_accessor :configuration
+      
+    end
+  end
+end
