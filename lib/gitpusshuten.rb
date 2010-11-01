91ed0aa1502c544dfb24195026a3d5b171a3f586
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-01 02:50:19 +0100

    Moved the GitPusshuTen::Initializer into a separate file.

--- lib/gitpusshuten.rb
+++ lib/gitpusshuten.rb
@@ -3,20 +3,4 @@ Dir[File.expand_path(File.join(File.dirname(__FILE__), 'gitpusshuten/**/*'))].ea
 end
 
 module GitPusshuTen
-  class Initializer
-    
-    def initialize(*args)
-      if not File.exist?(configuration_file)
-        puts "Could not locate the GitPusshuTen configuration file in #{configuration_file}"
-        exit
-      end
-      
-      GitPusshuTen::Configuration.new(:staging).parse!(configuration_file)
-    end
-    
-    def configuration_file
-      File.expand_path(File.join(Dir.pwd, '.gitpusshuten', 'config.rb'))
-    end
-    
-  end
 end
