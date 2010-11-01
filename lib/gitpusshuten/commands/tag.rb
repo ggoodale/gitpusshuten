41edda153255aa6489dcfc52ed0742444088e70e
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-01 22:18:30 +0100

    Added the initial setup for tag commands.

---
+++ lib/gitpusshuten/commands/tag.rb
@@ -0,0 +1,26 @@
+module GitPusshuTen
+  module Commands
+    class Tag < GitPusshuTen::Commands::Base
+            
+      ##
+      # Tag
+      attr_accessor :tag
+      
+      ##
+      # Initializes the Tag command
+      def initialize(cli, configuration)
+        @cli           = cli
+        @configuration = configuration
+        
+        @tag           = cli.arguments.shift
+      end
+      
+      ##
+      # Performs the Tag command
+      def perform!
+        
+      end
+      
+    end
+  end
+end
