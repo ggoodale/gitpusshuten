26c644bd320f0690794ac9f744b4c76a3a253db1
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-01 17:11:23 +0100

    Initial setup for the Initializer.

--- lib/gitpusshuten/initializer.rb
+++ lib/gitpusshuten/initializer.rb
@@ -1,15 +1,28 @@
 module GitPusshuTen
   class Initializer
     
+    ##
+    # Method to be called from CLI/Executable
     def initialize(*args)
       if not File.exist?(configuration_file)
-        puts "Could not locate the GitPusshuTen configuration file in #{configuration_file}"
+        GitPusshuTen::Log.error "Couldn't find the GitPusshuTen configuration file in #{configuration_file}."
         exit
       end
       
-      GitPusshuTen::Configuration.new(environment).parse!(configuration_file)
+      ##
+      # Parse the CLI arguments
+      cli = GitPusshuTen::CLI.new(args)
+      
+      ##
+      # Load in the requested environment and it's configuration
+      configuration = GitPusshuTen::Configuration.new(cli.environment).parse!(configuration_file)
+      
+      
+      
     end
     
+    ##
+    # Path to assumed configuration file
     def configuration_file
       File.expand_path(File.join(Dir.pwd, '.gitpusshuten', 'config.rb'))
     end
--- spec/initializer_spec.rb
+++ spec/initializer_spec.rb
@@ -1,7 +1,17 @@
 require 'spec_helper'
 
 describe GitPusshuTen::Initializer do
-  it "does something" do
-    
+  
+  before do
+    GitPusshuTen::Configuration.any_instance.stubs(:parse!)
   end
+  
+  it "should output an error if config file could not be found" do
+    GitPusshuTen::Initializer.any_instance.expects(:exit)
+    GitPusshuTen::Log.expects(:error)
+    GitPusshuTen::Initializer.new(%w[tag 1.4.2 to staging])
+  end
+  
+  
+  
 end
