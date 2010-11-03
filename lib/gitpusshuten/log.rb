c383d9745ba399be3b3f88a3b325aec7f77bf326
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-03 14:55:13 +0100

    Added comments and cleared whitespace.

--- lib/gitpusshuten/log.rb
+++ lib/gitpusshuten/log.rb
@@ -1,15 +1,22 @@
 module GitPusshuTen
   class Log
+    
+    ##
+    # Displays a regular message
     def self.message(message)
       puts "[message] ".color(:green) + message
     end
-    
+
+    ##
+    # Displays a warning message
     def self.warn(message)
       puts "[warning] ".color(:yellow) + message
     end
-    
+
+    ##
+    # Displays an error message
     def self.error(message)
       puts "[error] ".color(:red) + message
-    end    
+    end
   end
 end
