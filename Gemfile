4b449bf0d00fe655887fa7eb3db29ccfc597232a
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-03 16:59:41 +0100

    Added Bundler for Gem management.

---
+++ Gemfile
@@ -0,0 +1,10 @@
+source 'http://rubygems.org'
+
+gem 'rainbow'
+gem 'activesupport', :require => 'active_support/inflector'
+
+group :test do
+  gem 'rspec'
+  gem 'mocha'
+  gem 'infinity_test'
+end
--- lib/gitpusshuten.rb
+++ lib/gitpusshuten.rb
@@ -1,14 +1,6 @@
 require 'rubygems'
-
-begin
-  require 'rainbow'
-  require 'active_support/inflector'
-rescue LoadError
-  puts "Please ensure that you have the following gems installed:\n\n"
-  %w[activesupport rainbow].each do |gem|
-    puts "\s\s* #{gem}"
-  end
-end
+require 'bundler/setup'
+Bundler.require(:default)
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'gitpusshuten/**/*'))].each do |file|
   require file unless File.directory?(file)
