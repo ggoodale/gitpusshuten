9e7bd0c8a5ff64cd23ac2419fbe862e55c7b4e93
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-03 14:31:28 +0100

    Wrapper the gem requirement in a rescue block.

--- lib/gitpusshuten.rb
+++ lib/gitpusshuten.rb
@@ -1,6 +1,14 @@
 require 'rubygems'
-require 'rainbow'
-require 'active_support/inflector'
+
+begin
+  require 'rainbow'
+  require 'active_support/inflector'
+rescue LoadError
+  puts "Please ensure that you have the following gems installed:\n\n"
+  %w[activesupport rainbow].each do |gem|
+    puts "\s\s* #{gem}"
+  end
+end
 
 Dir[File.expand_path(File.join(File.dirname(__FILE__), 'gitpusshuten/**/*'))].each do |file|
   require file unless File.directory?(file)
