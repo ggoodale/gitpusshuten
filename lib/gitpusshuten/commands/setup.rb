01c22038311fc56ece478302e5360266ea2649c3
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-05 18:06:34 +0100

    Extended the setup command with the ability to add/re-add users on the remote environment.

--- lib/gitpusshuten/commands/setup.rb
+++ lib/gitpusshuten/commands/setup.rb
@@ -42,6 +42,50 @@ module GitPusshuTen
           GitPusshuTen::Log.message("The " + environment.name.to_s.color(:yellow) + " remote has been added:")
           GitPusshuTen::Log.message(configuration.user + '@' + configuration.ip + ':' + environment.application_root + "\n")
         end
+        
+        if object == 'user'
+          GitPusshuTen::Log.message "Confirming existance of user #{configuration.user.to_s.color(:yellow)} on the #{configuration.environment.to_s.color(:yellow)} environment."
+          if environment.user_exists?
+            GitPusshuTen::Log.message "It looks like #{configuration.user.to_s.color(:yellow)} already exists at #{configuration.ip.to_s.color(:yellow)}."
+            GitPusshuTen::Log.message "Would you like to remove and re-add #{configuration.user.to_s.color(:yellow)}?"
+            yes = choose do |menu|
+              menu.prompt = ''
+              menu.choice('Yes') { true  }
+              menu.choice('No')  { false }
+            end
+            if yes
+              GitPusshuTen::Log.message "Removing user #{configuration.user.to_s.color(:yellow)} from #{configuration.ip.to_s.color(:yellow)}."
+              if environment.remove_user!
+                GitPusshuTen::Log.message "Re-adding user #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}."
+                if environment.add_user!
+                  GitPusshuTen::Log.message "Successfully re-added #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}!"
+                else
+                  GitPusshuTen::Log.error "Failed to add user #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}."
+                  GitPusshuTen::Log.error "An error occurred."
+                end
+              else
+                GitPusshuTen::Log.error "Failed to remove user #{configuration.user.to_s.color(:yellow)} from #{configuration.ip.to_s.color(:yellow)}."
+                GitPusshuTen::Log.error "An error occurred."
+              end
+            end
+          else
+            GitPusshuTen::Log.message "It looks like #{configuration.user.to_s.color(:yellow)} does not yet exists."
+            GitPusshuTen::Log.message "Would you like to add #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}?"
+            yes = choose do |menu|
+              menu.prompt = ''
+              menu.choice('Yes') { true  }
+              menu.choice('No')  { false }
+            end
+            if yes
+              if environment.add_user!
+                GitPusshuTen::Log.message "Successfully added #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}!"
+              else
+                GitPusshuTen::Log.error "Failed to add user #{configuration.user.to_s.color(:yellow)} to #{configuration.ip.to_s.color(:yellow)}."
+                GitPusshuTen::Log.error "An error occurred."
+              end
+            end
+          end
+        end
       end
 
     end
--- lib/gitpusshuten/environment.rb
+++ lib/gitpusshuten/environment.rb
@@ -1,22 +1,22 @@
 module GitPusshuTen
   class Environment
-    
+
     ##
     # Stores the configuration
     attr_accessor :configuration
-    
+
     ##
     # Initializes the environment with the provided configuration
     def initialize(configuration)
       @configuration = configuration
     end
-    
+
     ##
     # Returns the name of the environment
     def name
       configuration.environment
     end
-    
+
     ##
     # Establishes a connection to the remote environment
     # to the user's home directory
@@ -26,7 +26,7 @@ module GitPusshuTen
         :port     => configuration.port
       }, &ssh)
     end
-    
+
     ##
     # Performs a single command on the remote environment
     # from the application root directory
@@ -35,19 +35,59 @@ module GitPusshuTen
         return environment.exec!("cd '#{application_root}'; #{command}")
       end
     end
-    
+
     ##
     # Returns the root of the application
     def application_root
       File.join(configuration.path, "#{sanitized_application_name}.#{configuration.environment}")
     end
-    
+
     ##
     # Takes the application name from the configuration and
     # replaces spaces with underscores and downcases capitalized characters
     def sanitized_application_name
       configuration.application.gsub(' ', '_').downcase
     end
-    
+
+    ##
+    # Checks the remote server to see if the provided user exists
+    def user_exists?
+      Net::SSH.start(configuration.ip, 'root') do |environment|
+        if(environment.exec!("grep '#{configuration.user}' /etc/passwd").nil?)
+          return false
+        else
+          return true
+        end
+      end
+    end
+
+    ##
+    # Adds a user to the remote server and sets the home directory
+    # to the path specified in the config.rb. This is the location
+    # to where applications will be deployed
+    def add_user!
+      Net::SSH.start(configuration.ip, 'root') do |environment|
+        response = environment.exec!("useradd -m --home='#{configuration.path}' --password='#{configuration.password}' '#{configuration.user}'")
+        if response.nil? or response =~ /useradd\: warning\: the home directory already exists\./
+          return true
+        else
+          return false
+        end
+      end
+    end
+
+    ##
+    # Removes a user from the remote server but does not remove
+    # the user's home directory since it might contain applications
+    def remove_user!
+      Net::SSH.start(configuration.ip, 'root') do |environment|
+        if environment.exec!("userdel '#{configuration.user}'").nil?
+          return true
+        else
+          return false
+        end
+      end
+    end
+
   end
 end
