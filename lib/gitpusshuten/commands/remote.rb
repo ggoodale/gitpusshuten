c4c6ca2fb62b94b8f1dee6dc71c52496a0f8a9b2
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-07 03:40:42 +0100

    Added Remote command.

---
+++ lib/gitpusshuten/commands/remote.rb
@@ -0,0 +1,32 @@
+module GitPusshuTen
+  module Commands
+    class Remote < GitPusshuTen::Commands::Base
+      description "Performs a command on the remote server for the specified environment from the application root."
+      usage       "remote '<command>' for <environment>"
+      example     "remote 'ls -la' for staging"
+      example     "remote 'mkdir tmp; touch tmp/restart.txt' for production"
+
+      ##
+      # Setup specific attributes/arguments
+      attr_accessor :command
+
+      ##
+      # Initializes the Delete command
+      def initialize(*objects)
+        super
+        
+        help if cli.arguments.empty? or environment.name.nil?
+        
+        @command = cli.arguments.join(' ')
+      end
+
+      ##
+      # Performs the Delete command
+      def perform!
+        GitPusshuTen::Log.message "Performing command on #{app_name} (#{environment_name}) at #{ip_addr}!"
+        environment.execute(command)
+      end
+
+    end
+  end
+end
