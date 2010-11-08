6e33a641f929c9d813b89b3d773bcdc9221076a6
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-08 21:08:32 +0100

    Added the Local class.

---
+++ lib/gitpusshuten/local.rb
@@ -0,0 +1,29 @@
+module GitPusshuTen
+  class Local
+    
+    ##
+    # Returns the .gitpusshuten local directory
+    def gitpusshuten_dir
+      File.join(Dir.pwd, '.gitpusshuten')
+    end
+
+    ##
+    # Returns the .gitpusshuten tmp directory
+    def tmp_dir
+      File.join(gitpusshuten_dir, 'tmp')
+    end
+
+    ##
+    # Create tmp_dir
+    def create_tmp_dir!
+      %x[mkdir -p '#{tmp_dir}']
+    end
+
+    ##
+    # Removes everything inside the tmp_dir
+    def clean_tmp_dir!
+      puts %x[rm -rf '#{tmp_dir}']
+    end
+    
+  end
+end
