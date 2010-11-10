2540acb105535a77e636a3ff97eb70ffea4487df
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-10 16:04:15 +0100

    Added the Bundler command.

---
+++ lib/gitpusshuten/modules/bundler/command.rb
@@ -0,0 +1,58 @@
+module GitPusshuTen
+  module Commands
+    class Bundler < GitPusshuTen::Commands::Base
+      description "[Module] Bundler commands."
+      usage       "bundler <command> for <environment>"
+      example     "bundler bundle for staging         # Installs Passenger with the NginX web server"
+
+      ##
+      # Passenger specific attributes/arguments
+      attr_accessor :command
+
+      ##
+      # Initializes the Bundler command
+      def initialize(*objects)
+        super
+        
+        @command = cli.arguments.shift
+        
+        help if command.nil? or e.name.nil?
+      end
+
+      ##
+      # Performs the Bundler command
+      def perform!
+        if respond_to?("perform_#{command}!")
+          send("perform_#{command}!")
+        else
+          GitPusshuTen::Log.error "Unknown RVM command: <#{y(command)}>"
+          GitPusshuTen::Log.error "Run #{y('gitpusshuten help bundler')} for a list bundler commands."
+        end
+      end
+      
+      def perform_bundle!
+        GitPusshuTen::Log.message "Bundling Gems for #{y(c.application)} (#{y(e.name)} environment)."
+        installed = e.installed?('bundle')
+        if not installed
+          GitPusshuTen::Log.message "Couldn't find Bundler, installing the gem."
+          Spinner.installing :message => nil, :return => true do
+            e.execute_as_user('gem install bundler --no-ri --no-rdoc')
+            installed = e.installed?('bundle')
+            if not installed
+              r("Unable to install Bundler.")
+            else
+              g("DONE")
+            end
+          end
+        end
+        
+        exit if not installed
+        
+        Spinner.installing :message => "Bundling Gems for #{y(c.application)}", :return => true do
+          puts e.execute_as_user('bundle install --without test development')
+        end
+      end
+
+    end
+  end
+end
