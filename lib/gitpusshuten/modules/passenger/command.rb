2cce06997493c22df19624825c61841468436bfc
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-15 14:07:38 +0100

    Corrected the Passenger description of the examples.

--- lib/gitpusshuten/modules/passenger/command.rb
+++ lib/gitpusshuten/modules/passenger/command.rb
@@ -3,8 +3,8 @@ module GitPusshuTen
     class Passenger < GitPusshuTen::Commands::Base
       description "[Module] Phusion Passenger commands."
       usage       "passenger <command> for <environment>"
-      example     "gitpusshuten passenger install for staging      # Installs Phusion Passenger with the NginX web server"
-      example     "gitpusshuten passenger update for staging       # Updates Phusion Passenger and the NginX web server if a new version is available"
+      example     "gitpusshuten passenger install for staging      # Installs Phusion Passenger with the NginX or Apache2 web server"
+      example     "gitpusshuten passenger update for staging       # Updates Phusion Passenger and the NginX or Apache2 web server if a new version is available"
       example     "gitpusshuten passenger restart for production   # Restarts the Passenger instance for the specified environment"
 
       ##
