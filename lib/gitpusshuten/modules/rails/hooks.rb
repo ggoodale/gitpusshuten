cc00704ffbd3242078b0f01bc0927eac5a525c5f
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-15 09:45:07 +0100

    Updated the Rails hooks.

--- lib/gitpusshuten/modules/rails/hooks.rb
+++ lib/gitpusshuten/modules/rails/hooks.rb
@@ -4,7 +4,8 @@
 # If it does not exist, it'll leave the database.yml unchanged.
 post "Setting proper database for environment. (Rails)" do
   run "if [[ -f 'config/database.yml.#{environment}' ]]; then " +
-      "echo 'config/database.yml.#{environment} found! Overwrite it over database.yml.';" +
+      "echo 'config/database.yml.#{environment} found! Using this one instead of config/database.yml';" +
+      "echo 'Overwriting config/database.yml with config/database.yml.#{environment}';" +
       "mv 'config/database.yml.#{environment}' 'config/database.yml'; " +
       "fi"
 end
