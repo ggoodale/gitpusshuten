ef52738dcd9ce73281323a1faf8a91a0592e4625
Michael van Rooijen <meskyanichi@gmail.com>
2010-11-16 01:25:21 +0100

    Nanoc now compiles and renames the output directory to public.

--- lib/gitpusshuten/modules/nanoc/hooks.rb
+++ lib/gitpusshuten/modules/nanoc/hooks.rb
@@ -3,6 +3,7 @@
 # Checks if nanoc has been installed before attempting to compile
 post "Compile HTML Files (Nanoc)" do
   run "if [[ -x $(which nanoc) ]]; then gem install nanoc; fi"
-  run "rm -rf output"
+  run "rm -rf output public"
   run "nanoc compile"
+  run "mv output public"
 end
